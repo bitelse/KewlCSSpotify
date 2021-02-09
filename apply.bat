6ba1e31545025799a521d9b6a30545806f158ae6
mightysword <jcraughton@gmail.com>
2021-02-09 02:55:05 -0600

    yay

---
+++ apply.bat
@@ -0,0 +1,5 @@
+spicetify config current_theme KewlCSSpotify color_scheme Base
+spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
+spicetify apply
+call %userprofile%\.spicetify\Themes\KewlCSSpotify\adblocker.bat
+pause
